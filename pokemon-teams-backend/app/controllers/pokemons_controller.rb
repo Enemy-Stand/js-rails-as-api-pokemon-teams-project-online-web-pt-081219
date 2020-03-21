99f49d706507314cb414c0d27383403a5473344e
Joseph Sesti <github email address>
2020-03-21 17:16:29 -0500

    Done.

--- pokemon-teams-backend/app/controllers/pokemons_controller.rb
+++ pokemon-teams-backend/app/controllers/pokemons_controller.rb
@@ -10,8 +10,11 @@ class PokemonsController < ApplicationController
   end
 
   def create
-    pokemon = Pokemon.new
-    if
+    if trainer.team_size < 6
+      Pokemon.create
+    else
+      flash.alert = "You already have 6 Pokemon."
+    end
   end
 
   def destroy
